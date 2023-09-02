0604418d78532cc4080f0a46e534d7cb67f938ed
NikosPol <polizopoulos96@gmail.com>
2023-09-02 16:17:46 +0300

    v0.2

--- src/main/java/gr/aueb/cf/schoolapp/dao/ITeacherDAO.java
+++ src/main/java/gr/aueb/cf/schoolapp/dao/ITeacherDAO.java
@@ -6,19 +6,46 @@ import java.util.List;
 
 public interface ITeacherDAO {
     /**
+     * Inserts a teacher to the database.
      *
      * @param teacher
+     *              the inserted teacher.
      * @return
+     *              the inserted teacher.
      */
     Teacher insert(Teacher teacher);
 
     /**
-     *
+     *  Updates a teacher from the database.
      * @param teacher
+     *              the teacher to be updated.
      * @return
+     *              the updated teacher.
      */
     Teacher update(Teacher teacher);
+
+    /**
+     * Deletes a teacher from the database.
+     * @param id
+     *              the id of the teacher to be deleted.
+     */
     void delete(Long id);
+
+    /**
+     * Returns a list of teachers by lastname.
+     * @param lastname
+     *              the given lastname.
+     * @return
+     *              the list of teachers.
+     */
     List<Teacher> getByLastname(String lastname);
+
+    /**
+     * Gets a teacher from database by id.
+     * @param id
+     *              the given id.
+     * @return
+     *              the selected teacher.
+     */
     Teacher getById(Long id);
 }
--- src/main/java/gr/aueb/cf/schoolapp/dao/TeacherDAOImpl.java
+++ src/main/java/gr/aueb/cf/schoolapp/dao/TeacherDAOImpl.java
@@ -3,6 +3,11 @@ package gr.aueb.cf.schoolapp.dao;
 import gr.aueb.cf.schoolapp.model.Teacher;
 import gr.aueb.cf.schoolapp.service.util.JPAHelper;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.ParameterExpression;
+import jakarta.persistence.criteria.Root;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.ext.Provider;
@@ -11,6 +16,13 @@ import java.util.List;
 @Provider
 @ApplicationScoped
 public class TeacherDAOImpl implements ITeacherDAO{
+    /**
+     * Inserts a teacher to the database.
+     * @param teacher
+     *              the inserted teacher.
+     * @return
+     *              the inserted teacher.
+     */
     @Override
     public Teacher insert(Teacher teacher) {
         EntityManager em = getEntityManager();
@@ -18,12 +30,24 @@ public class TeacherDAOImpl implements ITeacherDAO{
         return teacher;
     }
 
+    /**
+     * Updates a teacher from database.
+     * @param teacher
+     *              the teacher to be updated.
+     * @return
+     *              the updated teacher.
+     */
     @Override
     public Teacher update(Teacher teacher) {
         getEntityManager().merge(teacher);
         return teacher;
     }
 
+    /**
+     * Deletes a teacher from the database.
+     * @param id
+     *              the id of the teacher to be deleted.
+     */
     @Override
     public void delete(Long id) {
         EntityManager em = getEntityManager();
@@ -31,11 +55,33 @@ public class TeacherDAOImpl implements ITeacherDAO{
         em.remove(teacherToDelete);
     }
 
+    /**
+     * Returns a list of teachers by lastname.
+     * @param lastname
+     *              the given lastname.
+     * @return
+     *              the list of teachers.
+     */
     @Override
     public List<Teacher> getByLastname(String lastname) {
-        return null;
+        CriteriaBuilder builder = getEntityManager().getCriteriaBuilder();
+        CriteriaQuery<Teacher> selectQuery = builder.createQuery(Teacher.class);
+        Root<Teacher> root = selectQuery.from(Teacher.class);
+        ParameterExpression<String> tLastname = builder.parameter(String.class);
+        selectQuery.select(root).where(builder.like(root.get("lastname"),tLastname));
+
+        TypedQuery<Teacher> query = getEntityManager().createQuery(selectQuery);
+        query.setParameter(tLastname, lastname + "%");
+        return query.getResultList();
     }
 
+    /**
+     * Gets a teacher from database by id.
+     * @param id
+     *              the given id.
+     * @return
+     *              the selected teacher.
+     */
     @Override
     public Teacher getById(Long id) {
         EntityManager em = getEntityManager();
